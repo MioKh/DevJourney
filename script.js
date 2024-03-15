dc63d9bd3571fb15291dd75f1096d9d624658246
MioKh <mioakiyamakh@gmail.com>
2024-03-16 02:43:29 +0300

    callback hell

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function getData(endpoint) {
+function getData(endpoint, callback) {
   const xhr = new XMLHttpRequest();
 
   xhr.open("Get", endpoint);
 
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      const data = JSON.parse(this.responseText);
-      console.log(data);
+      callback(JSON.parse(this.responseText));
     }
   };
 
@@ -16,7 +15,13 @@ function getData(endpoint) {
   }, Math.floor(Math.random() * 3000) + 1000); // random seconds 1 to 3 random in miliseconds
 }
 
-getData("./users.json");
-getData("./fruits.json");
-getData("./vegetables.json");
-// this gets the data in random order if you want it to be in order you can use callbacks 
+getData("./users.json", (data) => {
+  console.log(data);
+  getData("./fruits.json", (data) => {
+    console.log(data);
+    getData("./vegetables.json", (data) => {
+      console.log(data);
+    });
+  });
+}); // now this gets data in order but it looks awful 
+// its even called a callback hell 
