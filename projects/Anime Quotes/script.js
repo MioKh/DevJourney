296244de95e4531cfc348b4d32eebbf9d147a42a
MioKh <mioakiyamakh@gmail.com>
2024-03-17 02:11:20 +0300

    replaced xhr with fetch

--- projects/Anime Quotes/script.js
+++ projects/Anime Quotes/script.js
@@ -4,18 +4,14 @@ const anime = document.querySelector(".anime");
 const quoteBtn = document.querySelector(".btn");
 
 function generateQuotes() {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://animechan.xyz/api/random", true); // credits https://github.com/rocktimsaikia/animechan
-  xhr.onload = function () {
-    if (this.status === 200) {
-      const data = JSON.parse(this.responseText);
-      text.innerHTML = data.quote;
-      character.innerHTML = `Character: ` + data.character;
-      anime.innerHTML = `Anime: ` + data.anime;
-    }
-  };
-  xhr.send();
+  fetch("https://animechan.xyz/api/random")
+  .then((response) => response.json())
+  .then((data) => { // a lot better then xhr 
+    text.innerHTML = data.quote;
+    character.innerHTML = `Character: ` + data.character;
+    anime.innerHTML = `Anime: ` + data.anime;
+  });
+
 } // a more practical example
 
 quoteBtn.addEventListener("click", generateQuotes);
-
