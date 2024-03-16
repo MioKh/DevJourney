b198a688eeeb2e0e2a8cccf0c3dab38b049d0bfb
MioKh <mioakiyamakh@gmail.com>
2024-03-16 03:00:32 +0300

    promise demo

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-function getData(endpoint, callback) {
-  const xhr = new XMLHttpRequest();
+// promises ( solves the problem of callback hell )
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+// for reference
 
-  xhr.open("Get", endpoint);
-
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      callback(JSON.parse(this.responseText));
-    }
-  };
-
+const getUser = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // simulate delay from server
-    xhr.send();
-  }, Math.floor(Math.random() * 3000) + 1000); // random seconds 1 to 3 random in miliseconds
-}
+    let err = false;
+    if (err) {
+      reject("error");
+    } else {
+      resolve({
+        name: "John",
+        age: 30,
+      });
+    }
+  }, 1000);
+});
 
-getData("./users.json", (data) => {
-  console.log(data);
-  getData("./fruits.json", (data) => {
+getUser
+  .then((data) => {
     console.log(data);
-    getData("./vegetables.json", (data) => {
-      console.log(data);
-    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-}); // now this gets data in order but it looks awful 
-// its even called a callback hell 
+
+console.log("hello from global scope");
