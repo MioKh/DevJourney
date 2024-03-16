cc684cf921ad49293d65eec5a972d65e14202ab9
MioKh <mioakiyamakh@gmail.com>
2024-03-17 02:07:31 +0300

    fetch api

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
-// using promise.all 
-// looks neater then promise chaining 
+// fetch api
+fetch("./users.json")
+  .then((response) => response.json())
+  .then((data) => console.log(data)); // more modern then xhr
 
-function getData(endpoint) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", endpoint);
-    xhr.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          resolve(JSON.parse(this.responseText));
-        } else {
-          reject(`something went wrong ${this.status}`);
-        }
-      }
-    };
-    setTimeout(() => {
-      xhr.send();
-    }, Math.floor(Math.random() * 3000) + 1000);
-  });
-}
-
-const userPromise = getData("./users.json");
-const fruitPromise = getData("./fruits.json");
-const vegetablePromise = getData("./vegetables.json");
-
-Promise.all([userPromise, fruitPromise, vegetablePromise])
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-  // looks better then promise chaining but this is optional at this point
+fetch("https://api.github.com/users/mioKH") // real api
+  .then((response) => response.json())
+  .then((data) => console.log(data.bio)); 
